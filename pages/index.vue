d21211c3b8be3324805579cd3a5346001ab4d732
KamilRizatdinov <k.rizatdinov@innopolis.university>
2020-09-09 21:29:26 +0300

    Change main page stylization

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,4 @@
 <template>
-  <v-app>
   <v-container fluid class="my-5">
     <v-row
       align="center"
@@ -14,35 +13,28 @@
       align="center"
       justify="center"
     >
-      <v-card class="d-flex align-content-center flex-wrap transparent" flat>
-        <h1 class="intro display-4">
-          Posts
-        </h1>
-      </v-card>
+      <h1 class="intro display-4">
+        Posts
+      </h1>
     </v-row>
-    <v-row 
-      align="center"
+
+    <v-row
+      class="cards-row"
+      dence
       justify="center"
+      align="center"
       >
-      <v-card
-        class="d-flex align-content-center transparent"
-        flat
-        min-height="200"
-        >
-        <v-flex xs12 sm6 md4 lg4 v-for="post in posts" :key="post.id">
-          <v-card class="text-center ma-2 " max-width="344" color="#241663">
-            <v-card-title class="justify-center" style="font-family: 'Poppins';font-style: normal;color: #EFFFFF; font-weight: 30; font-size: 140%;">
-              {{post.title}}
-            </v-card-title>
-            <v-card-text style="font-family: 'Poppins';font-style: normal;color: #EFFFFF;">
-              {{post.body}}
-            </v-card-text>
+          <v-card v-for="post in posts" @click="openPost(post.id)" :key="post.id" class="text-center ma-2 post-card" max-width="344">
+              <v-card-title class="justify-center" style="font-family: 'Poppins';font-style: normal; font-weight: 30; font-size: 140%;">
+                {{post.title}}
+              </v-card-title>
+              <v-card-text style="font-family: 'Poppins';font-style: normal;">
+                {{post.body}}
+              </v-card-text>
           </v-card>
-        </v-flex>
-      </v-card>
     </v-row>
+    
   </v-container>
-  </v-app>
 </template>
 
 <script>
@@ -66,5 +58,19 @@ export default {
       console.error(e)
     }
   },
+  methods: {
+    openPost(id) {
+      this.$router.push(`/${id}`)
+    }
+  },
 }
 </script>
+
+<style lang="scss" scoped>
+.post-card {
+  min-width: 200px;
+}
+.cards-row {
+  margin-top: 30px;
+}
+</style>
